f61e5c063e2ca3ab9c96eaf69b2565b6ec82ad6b
WadoG <WadoG@users.noreply.github.com>
2020-09-10 19:54:10 +0100

    commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+/*GALLERY*/
+
+function myFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    expandImg.src = imgs.src;
+    expandImg.parentElement.style.display = "flex";
+  }
